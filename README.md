298c7f4a865bd70029f5660d610e87e3d87a02e2
Christina Kozanian <christinakozanian@gmail.com>
2023-09-06 17:28:47 -0700

    Arrows now work and added text to README file

--- README.md
+++ README.md
@@ -1,1 +1,5 @@
-Christina Can Code!
+For my final project in my CS prep work, I've created my personal website which I've decided to name "Christina Can Code!"
+
+Here I have created a website linking both CSS and JS files in the HTML code. I've added a header. I've added a couple of photos of myself in the form of a slideshow with two arrows on the sides of the photo. I added a "click" event on the arrows for the user to be able look through the photos. Below that, there is some unordered and ordered text nested with each other, listing my interests. 
+
+I also used some styling to background colors, block color, font styling, and spacing.
--- script.js
+++ script.js
@@ -1,44 +1,41 @@
+
+
 const h2 = document.createElement("h2");
 h2.textContent = "My name is Christina Kozanian and here a few things I enjoy:";
 
 document.querySelector("body").appendChild(h2);
 
 
-// Add a click event listener to the "prev" arrow
-document.querySelector('.prev').addEventListener('click', function() {
-    plusSlides(-1); // Call the plusSlides function with -1 to show the previous slide
-});
+let slideIndex = 1; // Start with the first slide
 
-// Add a click event listener to the "next" arrow
-document.querySelector('.next').addEventListener('click', function() {
-    plusSlides(1); // Call the plusSlides function with 1 to show the next slide
-});
-
-let slideIndex = 0;
-showSlides();
-
-function plusSlides(n) {
-    showSlide(slideIndex += n);
-}
-
-function showSlides() {
-    showSlide(slideIndex);
-    slideIndex++;
-    if (slideIndex > slides.length) {
-        slideIndex = 1;
-    }
-}
-
+// Function to show a specific slide
 function showSlide(n) {
-    let slides = document.getElementsByClassName("slide");
-    if (n > slides.length) {
-        slideIndex = 1;
-    }
-    if (n < 1) {
-        slideIndex = slides.length;
-    }
-    for (let i = 0; i < slides.length; i++) {
-        slides[i].style.display = "none";
-    }
-    slides[slideIndex - 1].style.display = "block";
+  let slides = document.getElementsByClassName("slide");
+  if (n > slides.length) {
+    slideIndex = 1; // If at the end, start from the beginning
+  }
+  if (n < 1) {
+    slideIndex = slides.length; // If at the beginning, go to the end
+  }
+  for (let i = 0; i < slides.length; i++) {
+    slides[i].style.display = "none";
+  }
+  slides[slideIndex - 1].style.display = "block";
 }
+
+// Function to navigate to the previous slide
+function prevSlide() {
+  showSlide((slideIndex -= 1));
+}
+
+// Function to navigate to the next slide
+function nextSlide() {
+  showSlide((slideIndex += 1));
+}
+
+// Initial call to display the first slide
+showSlide(slideIndex);
+
+// Event listeners for prev and next buttons
+document.querySelector(".prev").addEventListener("click", prevSlide);
+document.querySelector(".next").addEventListener("click", nextSlide);
